9ac0856255b1cc1fa3cabbaaefff66c186da0cde
IndrePet <indrepetrausku@gmail.com>
2022-11-13 22:51:11 +0200

    moved secrets to environment variables

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import ejs from 'ejs';
-import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+
+dotenv.config();
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(
   bodyParser.urlencoded({
@@ -15,26 +19,31 @@ app.use(
   })
 );
 
-mongoose.connect('mongodb://127.0.0.1:27017/userDB', {
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
 
-const User = new mongoose.model('User', userSchema);
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
-app.get('/', (req, res) => {
-  res.render('home');
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app
-  .route('/register')
+  .route("/register")
   .get((req, res) => {
-    res.render('register');
+    res.render("register");
   })
   .post((req, res) => {
     const newUser = new User({
@@ -46,15 +55,15 @@ app
       if (err) {
         console.log(err);
       } else {
-        res.render('secrets');
+        res.render("secrets");
       }
     });
   });
 
 app
-  .route('/login')
+  .route("/login")
   .get((req, res) => {
-    res.render('login');
+    res.render("login");
   })
   .post((req, res) => {
     const username = req.body.username;
@@ -65,7 +74,7 @@ app
       } else {
         if (foundUser) {
           if (foundUser.password === password) {
-            res.render('secrets');
+            res.render("secrets");
           }
         }
       }
@@ -73,5 +82,5 @@ app
   });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });

9e749ebebc7a2903bf7a7c4d8ce7009e86927135
IndrePet <indrepetrausku@gmail.com>
2022-11-13 23:33:02 +0200

    password hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+import md5 from "md5";
 
 dotenv.config();
 
@@ -29,11 +29,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -48,7 +43,7 @@ app
   .post((req, res) => {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password,
+      password: md5(req.body.password),
     });
 
     newUser.save(err => {
@@ -67,7 +62,7 @@ app
   })
   .post((req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, (err, foundUser) => {
       if (err) {

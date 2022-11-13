4d5f2017b72d03fba984a49f02b6776ce0a6632c
IndrePet <indrepetrausku@gmail.com>
2022-11-13 20:23:19 +0200

    basic login

--- app.js
+++ app.js
@@ -1,25 +1,77 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
+app
+  .route('/register')
+  .get((req, res) => {
+    res.render('register');
+  })
+  .post((req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
 
-app.get('/register', (req, res) => {
-  res.render('register');
-});
+    newUser.save(err => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
+  });
+
+app
+  .route('/login')
+  .get((req, res) => {
+    res.render('login');
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, (err, foundUser) => {
+      if (err) {
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render('secrets');
+          }
+        }
+      }
+    });
+  });
 
 app.listen(3000, () => {
-  console.log('Server started');
+  console.log('Server started on port 3000');
 });
